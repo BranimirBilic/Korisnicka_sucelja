a8eeb12e9bc66dd1616dec66bafd18c83ed281b8
Branimir Bilic <brane@slicejack.com>
2019-10-23 22:27:04 +0200

    Added High fidelity

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,13 +14,14 @@ const IndexPage = () => ( <
     p > Welcome to your new Gatsby site. < /p> <
     p > Now go build something great. < /p> <
     div style = {
-        { maxWidth: `300px`, marginBottom: `1.45rem` } } >
+        { maxWidth: `300px`, marginBottom: `1.45rem` }
+    } >
     <
     Image / >
     <
     /div> <
-    Link to = "/page-2/" > Go to page 2 < /Link> <
-    /Layout>
+    Link to = "/page-2/" > Go to page 2 < /Link> < /
+    Layout >
 )
 
 export default IndexPage

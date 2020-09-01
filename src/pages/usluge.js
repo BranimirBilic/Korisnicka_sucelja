42014fc885f0f6fa157d54c76ac80621a15439ac
ivanbralic95 <ivanbralic95@gmail.com>
2020-09-01 16:58:16 +0200

    "Usluge.js" page with filtering

--- src/pages/usluge.js
+++ src/pages/usluge.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React , {useState}from "react";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/layout";
 import Img from "gatsby-image";
 import { SinglePost, PostInfo, PreTitle, Title, Subtitle, Update, Freshnew, Excrept, Card, CardColumns }  from "../components/style/emo-home-posts";
+import styles from './usluge.module.css'
 
 let minutes = 1000 * 60;
 let hours = minutes * 60;
@@ -10,10 +11,51 @@ let days = hours * 24;
 let months = days * 30;
 
 export default ({ data }) => {
+  const Products = data.allMarkdownRemark.edges;
+  
+  const [filterDisplay, setFilterDisplay] = useState(Products);
+
+  const handleChange = e => {
+    let oldProducts = [];
+    let newProducts = [];
+    
+    if(e !== "")
+    {
+      oldProducts = Products;
+
+      newProducts = oldProducts.filter(item => {   
+        //user input to lower case
+        const filter = e.toLowerCase();  
+      
+        //item title to lower case
+        const lc = item.node.frontmatter.title.toLowerCase();        
+
+        //tags array to lower case
+        const tagFilter = item.node.frontmatter.tags;
+        let tagFilterLC = tagFilter.map(item => item.toLowerCase())
+        let tagTrue = false;
+        tagFilterLC.forEach(item => {
+          if(item.includes(filter))
+            tagTrue = true;
+        })
+        
+        return (lc.includes(filter) || tagTrue);
+        }
+      )
+      setFilterDisplay(newProducts);
+    }
+    else
+    {
+      setFilterDisplay(Products);
+    }
+    }
+  
+
   return (
     <Layout>
+      <div className = {styles.SearchStyle}><input onChange = {e => handleChange(e.target.value)}></input></div>
       <CardColumns>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
+        {filterDisplay.map(({ node }) => (
           <Card key={node.id}>
             <Link to={node.fields.slug}>
               <SinglePost>
@@ -61,6 +103,7 @@ export const query = graphql`
             update(formatString: "DD MMMM, YYYY")
             title
             subtitle
+            tags
             featuredImage {
               childImageSharp {
                 fluid(maxWidth: 500, maxHeight: 500) {
